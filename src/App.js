0bf4d5684b1107770e887703c291f8e51ba2323f
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-09 01:35:30 +0100

    Netflix Clone fourth commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Row from "./Row"
+import requests from "./requests"
 
 function App() {
   return (
     <div className="App">
       <h1> Damian's Netflix clone</h1>
-      <Row title = "NETFLIX ORIGINALS" />
-      <Row title = "TRENDING NOW" />
+      <Row title = "NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />,
+      <Row title = "Trending Now" fetchUrl={requests.fetchTrending} />,
+      <Row title = "Top Rated" fetchUrl={requests.fetchTopRated} />,
+      <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies} />,
+      <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies} />,
+      <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies} />,
      
     </div>
   );
